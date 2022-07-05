aed2e5b21955cc45ace888745c9b8a4e7404dd7f
mecha2k <mecha2k@naver.com>
2022-07-05 09:00:02 +0900

    mnist

--- src/03 torch_tutorial_02.py
+++ src/03 torch_tutorial_02.py
@@ -16,7 +16,9 @@ print(f"{device} is available in torch")
 
 plt.style.use("seaborn")
 plt.rcParams["font.size"] = 16
-plt.rcParams["figure.dpi"] = 100
+plt.rcParams["figure.dpi"] = 200
+plt.rcParams["font.family"] = "NanumBarunGothic"
+plt.rcParams["axes.unicode_minus"] = False
 
 mnist_train = datasets.MNIST(
     root="./data",
@@ -50,7 +52,51 @@ for i in range(1, rows * cols + 1):
     index = np.random.randint(0, len(mnist_train), size=1)[0]
     image, label = mnist_train[index]
     fig.add_subplot(rows, cols, i)
-    plt.title(label)
+    plt.title(label, fontsize=18)
     plt.axis("off")
     plt.imshow(image.squeeze(), cmap="gray")
 plt.savefig("images/mnist_image")
+
+x = torch.randn(128, 20)
+model = nn.Linear(20, 10)
+y = model(x)
+print(y.shape)
+print(y)
+
+x = torch.randn(20, 16, 50, 100)
+model = nn.Conv2d(16, 33, kernel_size=3, stride=2)
+model = nn.Conv2d(16, 33, kernel_size=(3, 5), stride=(2, 1), padding=(1, 2))
+
+y = model(x)
+print(y.shape)
+
+layer = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=2).to(device)
+print(layer)
+
+weight = layer.weight
+print("weight:", weight.shape)
+
+print(images.shape)
+print(images[0].shape)
+
+image = images[0].squeeze()
+x = image.unsqueeze(dim=0)
+print("x:", x.shape)
+y = layer(x)
+print("y:", y.shape)
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 3, 1)
+plt.axis("off")
+plt.title("input", fontsize=18)
+plt.imshow(x[0, :, :].detach().cpu().numpy(), cmap="gray")
+plt.subplot(1, 3, 2)
+plt.axis("off")
+plt.title("output", fontsize=18)
+plt.imshow(y[0, :, :].detach().cpu().numpy(), cmap="gray")
+plt.subplot(1, 3, 3)
+plt.axis("off")
+plt.title("weight", fontsize=18)
+plt.imshow(weight[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
+plt.tight_layout()
+plt.savefig("images/mnist_conv")
