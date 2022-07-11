4f2fa8184caa8f9c7ee9d21c7dc9a7bb5cdf5a16
mecha2k <mecha2k@naver.com>
2022-07-11 14:52:44 +0900

    embedding words

--- src/torch-nlp/05 munging_ag_news.py
+++ src/torch-nlp/05 munging_ag_news.py
@@ -1,120 +1,56 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import collections
 import numpy as np
 import pandas as pd
 import re
-
 from argparse import Namespace
 
 
-# In[2]:
-
-
 args = Namespace(
-    raw_dataset_csv="data/ag_news/news.csv",
+    raw_dataset_csv="../data/ag_news/news.csv",
     train_proportion=0.7,
     val_proportion=0.15,
     test_proportion=0.15,
-    output_munged_csv="data/ag_news/news_with_splits.csv",
-    seed=1337
+    output_munged_csv="../data/ag_news/news_with_splits.csv",
+    seed=42,
 )
 
-
-# In[3]:
-
-
-# Read raw data
 news = pd.read_csv(args.raw_dataset_csv, header=0)
+print(news.info())
+print(news.head())
+print(set(news.category))
 
-
-# In[4]:
-
-
-news.head()
-
-
-# In[5]:
-
-
-# Unique classes
-set(news.category)
-
-
-# In[6]:
-
-
-# Splitting train by category
-# Create dict
 by_category = collections.defaultdict(list)
 for _, row in news.iterrows():
     by_category[row.category].append(row.to_dict())
 
-
-# In[7]:
-
-
-# Create split data
 final_list = []
 np.random.seed(args.seed)
 for _, item_list in sorted(by_category.items()):
     np.random.shuffle(item_list)
     n = len(item_list)
-    n_train = int(args.train_proportion*n)
-    n_val = int(args.val_proportion*n)
-    n_test = int(args.test_proportion*n)
-    
-    # Give data point a split attribute
+    n_train = int(args.train_proportion * n)
+    n_val = int(args.val_proportion * n)
+    n_test = int(args.test_proportion * n)
     for item in item_list[:n_train]:
-        item['split'] = 'train'
-    for item in item_list[n_train:n_train+n_val]:
-        item['split'] = 'val'
-    for item in item_list[n_train+n_val:]:
-        item['split'] = 'test'  
-    
-    # Add to final list
+        item["split"] = "train"
+    for item in item_list[n_train : n_train + n_val]:
+        item["split"] = "val"
+    for item in item_list[n_train + n_val :]:
+        item["split"] = "test"
     final_list.extend(item_list)
 
-
-# In[8]:
-
-
-# Write split data to file
 final_news = pd.DataFrame(final_list)
+print(final_news.split.value_counts())
 
 
-# In[9]:
-
-
-final_news.split.value_counts()
-
-
-# In[10]:
-
-
-# Preprocess the reviews
 def preprocess_text(text):
-    text = ' '.join(word.lower() for word in text.split(" "))
+    text = " ".join(word.lower() for word in text.split(" "))
     text = re.sub(r"([.,!?])", r" \1 ", text)
     text = re.sub(r"[^a-zA-Z.,!?]+", r" ", text)
     return text
-    
+
+
 final_news.title = final_news.title.apply(preprocess_text)
+print(final_news.head())
 
-
-# In[11]:
-
-
-final_news.head()
-
-
-# In[12]:
-
-
-# Write munged data to CSV
 final_news.to_csv(args.output_munged_csv, index=False)
-
--- src/torch-nlp/05 munging_frankenstein.py
+++ src/torch-nlp/05 munging_frankenstein.py
@@ -1,86 +1,59 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import os
-
-from argparse import Namespace
 import collections
 import nltk.data
 import numpy as np
 import pandas as pd
-import re
+import os, re
 import string
-from tqdm import tqdm_notebook
-
-
-# In[2]:
+from argparse import Namespace
+from tqdm import tqdm
 
 
 args = Namespace(
-    raw_dataset_txt="data/books/frankenstein.txt",
+    raw_dataset_txt="../data/books/frankenstein.txt",
     window_size=5,
     train_proportion=0.7,
     val_proportion=0.15,
     test_proportion=0.15,
-    output_munged_csv="data/books/frankenstein_with_splits.csv",
-    seed=1337
+    output_munged_csv="../data/books/frankenstein_with_splits.csv",
+    seed=42,
 )
 
-
-# In[3]:
-
-
-# Split the raw text book into sentences
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
 with open(args.raw_dataset_txt) as fp:
     book = fp.read()
+
 sentences = tokenizer.tokenize(book)
+print(len(sentences), "sentences")
+print("Sample:", sentences[100])
 
 
-# In[4]:
-
-
-print (len(sentences), "sentences")
-print ("Sample:", sentences[100])
-
-
-# In[5]:
-
-
-# Clean sentences
 def preprocess_text(text):
-    text = ' '.join(word.lower() for word in text.split(" "))
+    text = " ".join(word.lower() for word in text.split(" "))
     text = re.sub(r"([.,!?])", r" \1 ", text)
     text = re.sub(r"[^a-zA-Z.,!?]+", r" ", text)
     return text
 
 
-# In[6]:
-
-
 cleaned_sentences = [preprocess_text(sentence) for sentence in sentences]
-
-
-# In[7]:
-
-
-# Global vars
 MASK_TOKEN = "<MASK>"
 
-
-# In[8]:
-
-
-# Create windows
 flatten = lambda outer_list: [item for inner_list in outer_list for item in inner_list]
-windows = flatten([list(nltk.ngrams([MASK_TOKEN] * args.window_size + sentence.split(' ') +     [MASK_TOKEN] * args.window_size, args.window_size * 2 + 1))     for sentence in tqdm_notebook(cleaned_sentences)])
+windows = flatten(
+    [
+        list(
+            nltk.ngrams(
+                [MASK_TOKEN] * args.window_size
+                + sentence.split(" ")
+                + [MASK_TOKEN] * args.window_size,
+                args.window_size * 2 + 1,
+            )
+        )
+        for sentence in tqdm(cleaned_sentences)
+    ]
+)
 
-# Create cbow data
 data = []
-for window in tqdm_notebook(windows):
+for window in tqdm(windows):
     target_token = window[args.window_size]
     context = []
     for i, token in enumerate(window):
@@ -88,37 +61,24 @@ for window in tqdm_notebook(windows):
             continue
         else:
             context.append(token)
-    data.append([' '.join(token for token in context), target_token])
-    
-            
-# Convert to dataframe
+    data.append([" ".join(token for token in context), target_token])
+
 cbow_data = pd.DataFrame(data, columns=["context", "target"])
-
-
-# In[9]:
-
-
-# Create split data
 n = len(cbow_data)
+
+
 def get_split(row_num):
-    if row_num <= n*args.train_proportion:
-        return 'train'
-    elif (row_num > n*args.train_proportion) and (row_num <= n*args.train_proportion + n*args.val_proportion):
-        return 'val'
+    if row_num <= n * args.train_proportion:
+        return "train"
+    elif (row_num > n * args.train_proportion) and (
+        row_num <= n * args.train_proportion + n * args.val_proportion
+    ):
+        return "val"
     else:
-        return 'test'
-cbow_data['split']= cbow_data.apply(lambda row: get_split(row.name), axis=1)
+        return "test"
 
 
-# In[10]:
+cbow_data["split"] = cbow_data.apply(lambda row: get_split(row.name), axis=1)
+print(cbow_data.head())
 
-
-cbow_data.head()
-
-
-# In[11]:
-
-
-# Write split data to file
 cbow_data.to_csv(args.output_munged_csv, index=False)
-
